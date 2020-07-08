ab1d6c162125e47304ec46c20b8b5a7d2020d7a0
顾铭峰 <16450382@qq.com>
2020-07-08 10:49:24 +0800

    test IOS debug build

--- buildservice/cordovapack/util/buildApp/index.js
+++ buildservice/cordovapack/util/buildApp/index.js
@@ -2,10 +2,19 @@ import { cordova } from 'cordova-lib';
 
 function buildApp(platform, appBuildType) {
     var buildType = appBuildType === 'release';// ? true : platform == 'ios';
+    var options = {
+        "slilent": false,
+        "device": true,
+    };
+    if(buildType) {
+        options.release = true;
+    } else {
+        options.debug = true;
+    }
     return new Promise(function (resolve, reject) {
         cordova.build({
             platforms: [platform],
-            options: {"release": buildType, "silent": false, "device": true}
+            options: options,
         }, function (err, data) {
             if (err) {
                 reject(new Error(err))
